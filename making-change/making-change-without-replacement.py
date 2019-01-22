1278b7b0cd8ef54a64bce6818eaa7a3710695456
Ryan McMahon <rmcmahon@pivotal.io>
2019-01-21 16:49:43 -0800

    Making change without replacement DP

---
+++ making-change/making-change-without-replacement.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def making_change_without_replacement(v, X):
+    T = np.full([len(X),v], False, dtype=bool)
+
+    for i in range(len(X)):
+        for j in range(1,v+1):
+            if T[i-1,j-1] or \
+                (j-X[i] >= 0 and T[i-1,j-1-X[i]]) \
+                or X[i] == j:
+                T[i][j-1] = True
+
+    return T[len(X)-1][v-1]
+
+def main():
+    return 0
+
+if __name__ == "__main__":
+    X = [1,5,10,20]
+
+    for i in range(1,51):
+        print("Make change for " + str(i) + "?: " + str(making_change_without_replacement(i, X)) + "\n")
+
+    main()
